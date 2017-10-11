fc1be239ea883ce7d32fb31f2718ced384e38c13
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-11 18:50:50 -0500

    finished drill

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
+/* More Effecient */
+// const {ShoppingList, Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to ShoppingList
+// so there's some data to look at
+ShoppingList.create('beans', 2);
+ShoppingList.create('tomatoes', 3);
+ShoppingList.create('peppers', 4);
+
+
+//Recipes and Ingredients added
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+});
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
